793317a66a4923ebbec3891eb37b4b28de8bb6f7
hhw12409 <sksk2425@naver.com>
2022-06-11 21:01:32 +0900

    Feat : add main, freeboard link
    add FreeBoard component and MainPage component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import './App.css';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 function App() {
+
+  const navigate = useNavigate();
+
   return (
     <div className="App">
       <nav className='navbar'>
         <a href='/'>블로그</a>
         <ul>
-          <li><a href='/'>자유게시판</a></li>
+          <li><a onClick={()=> { navigate('/freeboard') }}>자유게시판</a></li>
         </ul>
       </nav>
-    </div>
+      <Routes>
+        <Route path='/' element={<MainPage/>}></Route>
+        <Route path='/freeboard' element={<FreeBoard/>}></Route>
+      
+      </Routes>
+    </div>    
   );
 }
 
+
+function FreeBoard() {
+  return (
+    <div>Freeboard page</div>
+  )
+}
+
+function MainPage() {
+  return (
+    <div>MainPage</div>
+  )
+}
 export default App;
