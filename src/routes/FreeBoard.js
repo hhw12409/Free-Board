ad63c1ec288fc17e89fc7fd333982173331c27d6
hhw12409 <sksk2425@naver.com>
2022-06-13 02:40:31 +0900

    Docs : Update Freeboard page

--- src/routes/FreeBoard.js
+++ src/routes/FreeBoard.js
@@ -1,10 +1,12 @@
 function FreeBoard() {
   return (
-    <div>
+    <div className='freeboard-page'>
       <h1>Title : Title test</h1>
       <p>Content : Content test</p>
       <p>date : 2022-06-12</p>
       <h3>Writer : Writer test</h3>
+      <button>Delete</button>
+      <button>Edit</button>
     </div>
   )
 }
