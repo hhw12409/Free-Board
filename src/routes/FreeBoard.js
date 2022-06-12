ae09f991fbedbfbc76452025ad4a108a94a504dd
hhw12409 <sksk2425@naver.com>
2022-06-12 16:39:02 +0900

    Feat : add NotFound and Freeborad page module

---
+++ src/routes/FreeBoard.js
@@ -0,0 +1,12 @@
+function FreeBoard() {
+  return (
+    <div>
+      <h1>Title : Title test</h1>
+      <p>Content : Content test</p>
+      <p>date : 2022-06-12</p>
+      <h3>Writer : Writer test</h3>
+    </div>
+  )
+}
+
+export default FreeBoard;
