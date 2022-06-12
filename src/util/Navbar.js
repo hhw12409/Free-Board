8f559e757668de35a82f3e025da1ef76d49d227a
hhw12409 <sksk2425@naver.com>
2022-06-12 16:35:38 +0900

    Feat : module Navbar file

--- src/util/Navbar.js
+++ src/util/Navbar.js
@@ -9,6 +9,8 @@ function Navbar() {
       <a href='/'>블로그</a>
       <ul>
         <li><a onClick={()=> { navigate('/freeboard') }}>자유게시판</a></li>
+        <li><a onClick={()=> { navigate('/writing') }}>글쓰기</a></li>
+        <li><a onClick={()=> { navigate('/register') }}>회원가입</a></li>
       </ul>
     </nav>
   )
