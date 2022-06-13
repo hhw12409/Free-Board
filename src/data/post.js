6ca6a51a0087d586352b9dead0224772fd848edf
Hyeonwoo <hhw12409@gmail.com>
2022-06-14 02:31:22 +0900

    Feat : add data file
    add post data file using JSON format

---
+++ src/data/post.js
@@ -0,0 +1,22 @@
+const post = [
+  {
+    title : '자유게시판 입니다.',
+    content : '안녕하세요 잘 부탁 드립니다.',
+    date : '2022-06-14',
+    writer : '노지연'
+  },
+  {
+    title : '반갑습니다. 여러분',
+    content : '잘부탁드립니다.',
+    date : '2022-06-14',
+    writer : '노홍자' 
+  },
+  {
+    title : '홍보 하겠습니다.',
+    content : '안녕하세요 홍보 하도록 하겠습니다.',
+    date : '2022-06-14',
+    writer : '노뿡자'
+  }
+]
+
+export default post;
