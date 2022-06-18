6ea0e1b72d66ca0707a4c15d404e0b9255994433
hhw12409 <sksk2425@naver.com>
2022-06-18 16:58:56 +0900

    Docs : edit input form
    mapping index, title, content, writer, value

--- src/routes/Edit.js
+++ src/routes/Edit.js
@@ -1,10 +1,19 @@
-function Edit() {
+function Edit(props) {
+
+  console.log(props.posts)
+
   return (
     <form className="write-form">
-      <input placeholder="Title" name="title"></input>
-      <input placeholder="Content" name="content"></input>
-      <input placeholder="Writer" name="writer"></input>
-      <input placeholder="Date" name="date"></input>
+      {
+        <>
+          <input type="hidden" value={props.posts.id}></input>
+          <input placeholder="Title" name="title" value={props.posts.title}></input>
+          <input placeholder="Content" name="content" value={props.posts.content}></input>
+          <input placeholder="Writer" name="writer" value={props.posts.writer}></input>
+          <input placeholder="Date" name="date" value={props.posts.date}></input>
+        </>
+      }
+      
       <div>
         <button>Edit</button>
       </div>
