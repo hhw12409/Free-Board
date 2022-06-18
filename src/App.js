59016413213c2afe138831e9d03d6d1db8ba7e3a
hhw12409 <sksk2425@naver.com>
2022-06-18 16:57:40 +0900

    Feat : add edit url mapping index

--- src/App.js
+++ src/App.js
@@ -7,18 +7,28 @@ import FreeBoard from './routes/FreeBoard';
 import Writing from './routes/Writing';
 import Register from './routes/Register';
 import Edit from './routes/Edit';
+import post from './data/post';
+import { useState } from 'react';
 
 function App() {
+
+  const [posts, setPosts] = useState(post);
 
   return (
     <div className="App">
       <Navbar/>
       <Routes>
         <Route path='/' element={<MainPage/>}/>
-        <Route path='/freeboard' element={<FreeBoard/>}/>
+        <Route path='/freeboard' element={<FreeBoard posts={ posts }/>}/>
         <Route path='/writing' element={<Writing/>}/>
         <Route path='/register' element={<Register/>}/>
-        <Route path='/edit' element={<Edit/>}/>
+        {
+          posts.map((_, index) => {
+            return (
+              <Route path={`/edit/${index}`} element={<Edit posts={ posts[index] }/>}/>
+            )
+          })
+        }
         <Route path='*' element={<NotFound/>}/>
       </Routes>
     </div>    
